bfe9384bce19e2a3e8d7954400029ca4eadf411e
kiko-g <kikojpgoncalves@gmail.com>
2021-11-22 03:21:51 +0000

    mulBN done | need to fix sum/sub carry

--- haskell/project/BigNumber.hs
+++ haskell/project/BigNumber.hs
@@ -12,29 +12,49 @@ scanner str = map (\c -> read [c :: Char] :: Int) str
 output :: BigNumber -> String
 output = concatMap show
 
--- 2.4
-pad :: BigNumber -> Int -> BigNumber
-pad (x : xs) n
-  | x < 0 = x : pad xs n
-pad x n
-  | length x < n = pad (0 : x) n
+-- 2.4 +++ need to fix carry for sum/sub
+padLeftZeros :: BigNumber -> Int -> BigNumber
+padLeftZeros (x : xs) n
+  | x < 0 = x : padLeftZeros xs n
+padLeftZeros x n
+  | length x < n = padLeftZeros (0 : x) n
   | otherwise = x
 
 somaBN :: BigNumber -> BigNumber -> BigNumber
 somaBN x y
   | length x == length y = zipWith (+) x y
-  | length x > length y = zipWith (+) x (pad y (length x))
-  | otherwise = zipWith (+) (pad x (length y)) y
+  | length x > length y = zipWith (+) x (padLeftZeros y (length x))
+  | otherwise = zipWith (+) (padLeftZeros x (length y)) y
 
 -- 2.5
 subBN :: BigNumber -> BigNumber -> BigNumber
 subBN x y
   | length x == length y = zipWith (-) x y
-  | length x > length y = zipWith (-) x (pad y (length x))
-  | otherwise = zipWith (-) (pad x (length y)) y
+  | length x > length y = zipWith (-) x (padLeftZeros y (length x))
+  | otherwise = zipWith (-) (padLeftZeros x (length y)) y
 
 -- 2.6
--- mulBN :: BigNumber -> BigNumber -> BigNumber
+algarismos :: Int -> BigNumber
+algarismos n = reverse (algarismosRev n)
+
+algarismosRev :: Int -> BigNumber
+algarismosRev 0 = []
+algarismosRev n = n `mod` 10 : algarismosRev (n `div` 10)
+
+padMulDivAux :: BigNumber -> Int -> BigNumber
+padMulDivAux [] _ = []
+padMulDivAux (x : xs) i = padMulDivAux xs (i + 1) ++ [x * 10 ^ i]
+
+padMulDiv :: BigNumber -> BigNumber
+padMulDiv bn = padMulDivAux (reverse bn) 0
+
+mulBNAux1 :: BigNumber -> BigNumber -> BigNumber
+mulBNAux1 [] _ = []
+mulBNAux1 _ [] = []
+mulBNAux1 xs (y : ys) = somaBN (algarismos (sum (zipWith (*) (replicate (length xs) y) xs))) (mulBNAux1 xs ys)
+
+mulBN :: BigNumber -> BigNumber -> BigNumber
+mulBN x y = mulBNAux1 (padMulDiv x) (padMulDiv y)
 
 -- 2.7
 -- divBN :: BigNumber -> BigNumber -> (BigNumber, BigNumber)
